fc75e6c8afa6cbc6d891cd5830ed9b87569bb26e
mimul <mimul@fittobe.com>
2026-07-13 16:18:30 +0900

    fix: 홈 Latest Snippets 카드 영역 비율 고정
    - 3개 카드 동일 높이 (column flex stretch)
    - 타이틀 1줄 말줄임, description 3줄 고정 공간(min-height) + 말줄임
    - description이 1~2줄이어도 카드 전체 크기 동일 유지

--- src/sections/home/home-snippets.js
+++ src/sections/home/home-snippets.js
@@ -2,6 +2,8 @@ import TitleBar from "@components/title-bar";
 import React from "react";
 import SnippetRollItem from "@components/snippet-roll-item";
 import { chunk } from "lodash";
+import styled from "@emotion/styled";
+import { css } from "@emotion/react";
 
 const HomeSnippets = ({ snippets }) => {
   if (!snippets || snippets.length === 0) {
@@ -29,13 +31,44 @@ const HomeSnippets = ({ snippets }) => {
     );
   });
   return (
-    <section className="section">
+    <HomeSnippetsBody className="section">
       <div className="container">
         <TitleBar title="Latest Snippets" linkTo="/snippets/" />
         {rowElements}
       </div>
-    </section>
+    </HomeSnippetsBody>
   );
 };
 
 export default HomeSnippets;
+
+const HomeSnippetsCss = (p) => css`
+  .columns {
+    align-items: stretch;
+  }
+  .column {
+    display: flex;
+    .box {
+      flex: 1;
+      width: 100%;
+    }
+    .post-title {
+      display: block;
+      white-space: nowrap;
+      overflow: hidden;
+      text-overflow: ellipsis;
+    }
+    p.ar-subtitle {
+      line-height: 1.6;
+      min-height: 4.8em;
+      display: -webkit-box;
+      -webkit-line-clamp: 3;
+      -webkit-box-orient: vertical;
+      overflow: hidden;
+    }
+  }
+`;
+
+const HomeSnippetsBody = styled.section`
+  ${HomeSnippetsCss}
+`;
